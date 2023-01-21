0564466215842ce3e095bea10a32214dd0347164
andres <andresflobolugo00@gmail.com>
2023-01-21 19:30:42 -0400

    cambios

--- src/app/modules/about/About.js
+++ src/app/modules/about/About.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import TittleImage from '../../shared/tittleImg/TittleImage'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <TittleImage></TittleImage>
+      <p>about</p>
+    
+    </div>
   )
 }
---
+++ src/app/shared/tittleImg/TittleImage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TittleImage.css'
+
+export default function TittleImage({tittle,rute}) {
+  return (
+    <div className='bg-img-1 d-flex flex-column justify-content-center align-items-center'>
+        
+        
+        <h2>titulo de prueba</h2>
+        <p>ruta</p>
+        
+       
+    
+    </div>
+  )
+}
