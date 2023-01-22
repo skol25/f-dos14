18641fc17ce6c50faa6d2a4c6c36452472f5e1aa
skol25 <andres.lobolugo25@gmail.com>
2023-01-22 00:17:13 -0400

    imagenes y

--- src/app/modules/home/Home.js
+++ src/app/modules/home/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
 import './Home.css'
+import TittleImage from '../../shared/tittleImg/TittleImage'
+
 
 export default function Home() {
+
+  let dosInfo = [
+    {
+      tittle:'DISEÑO',
+      icon:'',
+      description:'Diseño estructural de acero y concreto de obras civiles.  Conceptualización, Bocetos, Desarrollo del diseño, Integración, exposición al cliente y construcción de documentos. '
+    }
+  ]
+
+
+
+
   return (
-    <div>Home</div>
+    <div>
+      {/* titulo con imagen */}
+      <TittleImage></TittleImage>
+      {/*diseño obras y servicios seccion */}
+      <div>
+
+
+
+
+
+      </div>
+
+
+
+      
+
+
+    </div>
   )
 }
--- src/app/shared/tittleImg/TittleImage.js
+++ src/app/shared/tittleImg/TittleImage.js
@@ -3,7 +3,7 @@ import './TittleImage.css'
 
 export default function TittleImage({tittle,rute}) {
   return (
-    <div className='bg-img-1 d-flex flex-column justify-content-center align-items-center'>
+    <div className='bg-img-home d-flex flex-column justify-content-center align-items-center'>
         
         
         <h2>titulo de prueba</h2>
