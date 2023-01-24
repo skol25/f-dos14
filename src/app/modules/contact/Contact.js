31eb6ebee8115ff35b20f5423159019fe588147d
andres <andresflobolugo00@gmail.com>
2023-01-24 13:21:03 -0400

    componente contacto casi listo

--- src/app/modules/contact/Contact.js
+++ src/app/modules/contact/Contact.js
@@ -1,7 +1,104 @@
 import React from 'react'
+import TittleImage from '../../shared/tittleImg/TittleImage'
+import mailIcon from '../../../assets/images/mailIcon.svg'
+import addresIcon from '../../../assets/images/addresIcon.svg'
+import phoneicon from '../../../assets/images/phoneicon.svg'
+import SolariumContactImage from '../../../assets/images/SolariumContactImage.svg'
+import blueLine from '../../../assets/images/blueLine.svg'
+import FormComponent from '../../shared/formComponent/formComponent'
+
+import './Contact.css'
+
+
+
+
+
 
 export default function Contact() {
+
+  let contactInfo=[
+    {
+      icon:mailIcon,
+      tittle:'Correo',
+      description:'Constructorados14@gmail.com'
+    },
+    {
+      icon:addresIcon,
+      tittle:'Dirección',
+      description:'Sector Loma Gorda, Conj. Res. Loma Alta II, Nivel 4, apto 22, Carrizal, Edo. Miranda '
+    }, 
+    {
+      icon:phoneicon,
+      tittle:'Números Telefónicos',
+      description:'+58-414-2662055 +58-414-1307828'
+    }
+
+
+  ]
+
+
   return (
-    <div>Contact</div>
+    <div>
+      <TittleImage/>
+
+      {/*info de contacto  */}
+      <div className='container mb-4'> 
+
+      <div className='bg-blue color-white  mt-4 mb-4'>
+          <div className='row pt-2'>
+          {contactInfo && contactInfo?.map(e=>{
+
+          return(
+            <div key={e.tittle} className='d-flex col-4 justify-content-center'>
+              <div>
+                <img src={e.icon} alt='icono mail'/>
+              </div>
+              <div>
+                <h3>{e.tittle}</h3>
+                <p>{e.description}</p>
+              </div>
+            </div>
+          )
+
+          })}
+        
+        </div>
+      
+      </div>
+       
+
+
+
+         {/*info de contacto  */}
+      
+      <div className='row p-0 m-0' >
+      <div className='col-6 p-0 '>
+      
+        <FormComponent />
+      
+      </div>
+        <div className='col-1 v-line-blue'>
+         
+        
+        </div>
+        <div className='col-5 p-0'>
+          <img src={SolariumContactImage}/>
+        
+        </div>
+        
+      </div>
+    
+    
+
+
+
+
+
+      </div>
+     
+
+
+
+    </div>
   )
 }
