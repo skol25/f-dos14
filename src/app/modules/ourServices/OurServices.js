f4e03cab9dcb441e3479a44cbf2d72c5b23adc91
skol25 <andres.lobolugo25@gmail.com>
2023-01-24 11:13:44 -0400

    commit

--- src/app/modules/ourServices/OurServices.js
+++ src/app/modules/ourServices/OurServices.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import TittleImage from '../../shared/tittleImg/TittleImage'
+import OurServiceCard from '../../shared/ourServiceCard/ourServiceCard'
+
 
 export default function OurServices() {
   return (
-    <div>OurServices</div>
+    <div>
+      {/* titulo con imagen */}
+      <TittleImage/>
+
+      {/* Pequeña descrripcion de lo que hacen  */}
+      <OurServiceCard />
+
+
+    </div>
   )
 }
